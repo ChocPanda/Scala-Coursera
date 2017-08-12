64b966145530c118350aa192d7e76098fdacb9b1
matt <matt.searle@ocado.com>
2017-08-12 16:14:00 +0100

    Add existing work

---
+++ Part_1/Week_1-1/example/src/main/scala/example/Lists.scala
@@ -0,0 +1,65 @@
+package example
+
+
+object Lists {
+
+  /**
+    * This method computes the sum of all elements in the list xs. There are
+    * multiple techniques that can be used for implementing this method, and
+    * you will learn during the class.
+    *
+    * For this example assignment you can use the following methods in class
+    * `List`:
+    *
+    *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+    *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+    * is empty an exception is thrown
+    *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+    * list `xs` without its `head` element
+    *
+    * ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+    * solution.
+    *
+    * @param xs A list of natural numbers
+    * @return The sum of all elements in `xs`
+    */
+  def sum(xs: List[Int]): Int = {
+    def recSum(curr: Int, list: List[Int]): Int = {
+      if (list.isEmpty) {
+        curr
+      } else {
+        recSum(curr + list.head, list.tail)
+      }
+    }
+
+    recSum(0, xs)
+  }
+
+  /**
+    * This method returns the largest element in a list of integers. If the
+    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+    *
+    * You can use the same methods of the class `List` as mentioned above.
+    *
+    * ''Hint:'' Again, think of a recursive solution instead of using looping
+    * constructs. You might need to define an auxiliary method.
+    *
+    * @param xs A list of natural numbers
+    * @return The largest element in `xs`
+    * @throws java.util.NoSuchElementException if `xs` is an empty list
+    */
+  def max(xs: List[Int]): Int = {
+    def recMax(currMax: Int, list: List[Int]): Int = {
+      list match {
+        case Nil => currMax
+        case head :: tail => recMax(math.max(currMax, head), tail)
+      }
+    }
+
+    if (xs.isEmpty) {
+      throw new java.util.NoSuchElementException("list: xs is empty")
+    }
+
+    recMax(xs.head, xs.tail)
+  }
+}
