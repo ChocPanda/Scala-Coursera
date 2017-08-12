099de7009592f0136a6e483cb3ba959614c12a65
matt <matt.searle@ocado.com>
2017-08-12 16:19:58 +0100

    saved UNDO information for reset changes and discarded files

--- Part_1/Week_1-1/example/src/main/scala/example/Main.scala
+++
@@ -1,8 +0,0 @@
-package example
-
-import example.Lists._
-
-object Main extends App {
-  println("max(List(1, 45, 32, 45, 76, 4)) = " + max(List(1, 45, 32, 45, 26, 4)))
-  println("sum(List(1, 45, 32, 45, 76, 4)) = " + sum(List(1, 45, 32, 45, 76, 4)))
-}
--- Part_1/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "Part_1"
-
-version := "1.0"
-
-scalaVersion := "2.12.1"
-        
